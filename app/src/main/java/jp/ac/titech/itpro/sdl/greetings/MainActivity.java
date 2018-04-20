625ca2948b6812023ef6ee1e93211ed7ee4a049f
Takuo Watanabe <takuo@acm.org>
2018-04-20 11:26:23 +0900

    Made rotatable

--- app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
@@ -2,6 +2,7 @@ package jp.ac.titech.itpro.sdl.greetings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,15 +10,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "MainActivity";
+
     private TextView outputView;
     private EditText inputName;
 
-    private String name = "";
+    private final static String KEY_NAME = "MainActivity.name";
+    private String name = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null)
+            name = savedInstanceState.getString(KEY_NAME);
+
         outputView = findViewById(R.id.output_view);
         inputName = findViewById(R.id.input_name);
         Button okButton = findViewById(R.id.ok_button);
@@ -25,10 +34,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 name = inputName.getText().toString().trim();
-                if (name.length() > 0)
-                    outputView.setText(getString(R.string.output_view_format, name));
+                greet();
             }
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        greet();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putString(KEY_NAME, name);
+    }
+
+    private void greet() {
+        if (name != null && name.length() > 0)
+            outputView.setText(getString(R.string.output_view_format, name));
+    }
 }
